f39a0d6d000a163c6b12250a8e5a9baa1a8cee48
Mingu Kim <mingufkim@gmail.com>
2024-03-13 23:25:41 +0900

    ➕ Firebase

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDPQgS07oyRwUmminmW7ug0C0mdXvWb4Ec',
+  authDomain: 'kappa-70bf0.firebaseapp.com',
+  projectId: 'kappa-70bf0',
+  storageBucket: 'kappa-70bf0.appspot.com',
+  messagingSenderId: '562136695365',
+  appId: '1:562136695365:web:55fc7f77791753cb08592c',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
